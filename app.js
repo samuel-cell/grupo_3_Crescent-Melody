2ccb90d4a53a36fe563a332729b8a2387c8845f9
faacku010 <fachundo20@gmail.com>
2023-08-02 01:28:14 -0300

    agregando datos archivo app.js

--- app.js
+++ app.js
@@ -10,14 +10,22 @@ app.listen(3015,() => {
     console.log('servidor corriendo en puerto 3015');
 });
 
-app.get('/', (req, res) => {
+app.get('/Home', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/index.html'));
 })
 
-app.get('/product-detail', (req, res) => {
+app.get('/Product-detail', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/productDetail.html'));
 })
 
-app.get('/product-cart', (req, res) => {
+app.get('/Product-cart', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/productCart.html'));
+})
+
+app.get('/Login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/login.html'));
+})
+
+app.get('/Register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './src/views/register.html'));
 })
