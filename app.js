d47ca2c482136f352f08223101cb7e1b26b13360
faacku010 <fachundo20@gmail.com>
2023-08-04 01:25:22 -0300

    agregando estilo y modificaciones detalleCarrito

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(3015,() => {
     console.log('servidor corriendo en puerto 3015');
 });
 
-app.get('/Home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './src/views/index.html'));
 })
 
